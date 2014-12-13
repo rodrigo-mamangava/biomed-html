da5695c8419429ba7f0c9612b0e4521a724ac156
rodrigo-mamangava <rodrigo@mamangava.com>
2014-12-13 15:28:57 -0200

    First push
    Primeiro push do projeto

---
+++ _header.php
@@ -0,0 +1,60 @@
+<header class="container">
+	<nav class="navbar navbar-default " role="navigation">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Biomed</a>
+	    </div>
+	
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      	      
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="#">Empresa</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+	          	Produtos
+	          	<span class="caret"></span>
+	          </a>
+	          <ul class="dropdown-menu dropdown-menu-left" role="menu">
+	            <li><a href="#">Produto 01</a></li>
+	            <li><a href="#">Produto 02</a></li>
+	            <li><a href="#">Produto 03</a></li>
+	            <li><a href="#">Produto 04</a></li>
+	          </ul>
+	        </li>
+	        <li><a href="#">Suporte</a></li>
+	        <li><a href="#">Eventos</a></li>
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+	          	Contato
+	          	<span class="caret"></span>
+	          </a>
+	          <ul class="dropdown-menu dropdown-menu-left" role="menu">
+	            <li><a href="#">Fale conosco</a></li>
+	            <li><a href="#">Trabalhe conosco</a></li>
+	          </ul>
+	        </li>
+	        <li>
+		        <form class="navbar-form navbar-right" role="search">
+			        <div class="input-group">
+				      <input type="text" class="form-control">
+				      <span class="input-group-btn">
+				        <button class="btn btn-default" type="button">Go!</button>
+				      </span>
+				    </div><!-- /input-group -->
+			   </form>
+	        </li>
+	      </ul>	      
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav><!-- /nav -->
+	
+</header> <!-- /header -->
+
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="language" content="pt-br" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="css/style.css">
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src=></script>
+
+<title>BIOMED HTML</title>
+</head>
+<body>
+
+<?php include '_header.php' ;?>
+
+
+<?php include '_img-destaque-home.php' ;?>
+
+
+<div class="box-col-03"></div> <!-- /box-col-03 -->
+
+<div class="divisoria-tipo-01"></div> <!-- /divisoria-tipo-01 -->
+
+<div class="vitrine-tipo-home"></div> <!-- /vitrine-tipo-home -->
+
+<div class="form-simples"></div> <!-- /form-simples -->
+
+<div class="divisoria-tipo-01"></div> <!-- /divisoria-tipo-01 -->
+
+<div class="vitrine-parceiros"></div> <!-- /vitrine-parceiros -->
+
+<footer></footer> <!-- /footer -->
+
+
+</body>
+</html>
