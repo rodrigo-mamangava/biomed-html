29dd48e47e28746e5a30da2fec24d25444f4af1b
rodrigo-mamangava <rodrigo@mamangava.com>
2014-12-13 16:54:49 -0200

    Home  & Alteração dos breakpoints
    - _header
    - _img-destaque-home
    - box-col-03
    - divisoria-tipo-01
    - vitrine-tipo-home
    
    E alteracao dos breackpoints

--- index.php
+++ index.php
@@ -23,19 +23,79 @@
 <?php include '_img-destaque-home.php' ;?>
 
 
-<div class="box-col-03"></div> <!-- /box-col-03 -->
+<div class="box-col-03 container">
+		<div class="row">
+			<div class="col-sm-4">
+				<h2>Aqui vai um titulo H2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+					quis massa tincidunt, pulvinar arcu eget, euismod arcu. Nulla massa
+					quam, gravida in arcu at, mattis mollis tellus.</p>
+			</div> <!-- col-sm-4 -->
+			<div class="col-sm-4">
+				<h2>Aqui vai um titulo H2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+					quis massa tincidunt, pulvinar arcu eget, euismod arcu. Nulla massa
+					quam, gravida in arcu at, mattis mollis tellus.</p>
+			</div> <!-- col-sm-4 -->
+			<div class="col-sm-4">
+				<h2>Aqui vai um titulo H2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+					quis massa tincidunt, pulvinar arcu eget, euismod arcu. Nulla massa
+					quam, gravida in arcu at, mattis mollis tellus.</p>
+			</div> <!-- col-sm-4 -->
+		</div>
+		<!-- row -->
+	</div>
+	<!-- /box-col-03 -->
 
-<div class="divisoria-tipo-01"></div> <!-- /divisoria-tipo-01 -->
+	<div class="divisoria-tipo-01 container-fluid">
+		<div class="row">
+		
+			<div class="col-xs-4 col-md-5 col-esq">
+				<p><hr></p>
+			</div>
+			<div class="col-xs-4 col-md-2">
+				<p> Produtos em destaque</p>
+			</div>
+			<div class="col-xs-4 col-md-5 col-dir">
+				<p><hr></p>
+			</div>
+		
+		</div>
+	</div>
+	<!-- /divisoria-tipo-01 -->
 
-<div class="vitrine-tipo-home"></div> <!-- /vitrine-tipo-home -->
+	<div class="vitrine-tipo-home container">
+			<div class="row">
+			<div class="col-sm-3">
+				<img src="img/logo/primeiro.jpg" class="logo-parceiros">
+			</div> <!-- col-sm-3 -->
+			<div class="col-sm-3">
+				<img src="img/logo/primeiro.jpg" class="logo-parceiros">
+			</div> <!-- col-sm-3 -->
+			<div class="col-sm-3">
+				<img src="img/logo/primeiro.jpg" class="logo-parceiros">				
+			</div> <!-- col-sm-3 -->
+			<div class="col-sm-3">
+				<img src="img/logo/primeiro.jpg" class="logo-parceiros">			
+			</div> <!-- col-sm-3 -->
+		</div>
+		<!-- row -->
+	
+	</div>
+	<!-- /vitrine-tipo-home -->
 
-<div class="form-simples"></div> <!-- /form-simples -->
+	<div class="form-simples"></div>
+	<!-- /form-simples -->
 
-<div class="divisoria-tipo-01"></div> <!-- /divisoria-tipo-01 -->
+	<div class="divisoria-tipo-01"></div>
+	<!-- /divisoria-tipo-01 -->
 
-<div class="vitrine-parceiros"></div> <!-- /vitrine-parceiros -->
+	<div class="vitrine-parceiros"></div>
+	<!-- /vitrine-parceiros -->
 
-<footer></footer> <!-- /footer -->
+	<footer></footer>
+	<!-- /footer -->
 
 
 </body>
