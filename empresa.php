4a51af244d06dc459337f32a61f670b33beeb035
rodrigo-mamangava <rodrigo@mamangava.com>
2014-12-15 02:10:42 -0200

    Page Empresa - v00
    Versão 00 da página EMPRESA

---
+++ empresa.php
@@ -0,0 +1,154 @@
+<?php include '_header.php';?>
+
+<?php include '_barra-local.php';?>
+
+
+<div class="box-03-titulo container">
+	<div class="row titulo-box-03">
+		<div class="col-sm-12">
+			<h3>Desde 1969 o melhor suporte aos médicos brasileiros</h3>
+		</div>
+	</div>
+	<div class="row texto-box-03">
+		<div class="col-sm-4">
+			In posuere dictum diam sed pretium. 
+			Donec ac aliquet justo. Nullam dictum maximus nibh, vitae interdum enim tempus tristique. 
+			Nullam sed erat nulla. Etiam quis dui ac felis vehicula ornare.
+		</div>
+		<div class="col-sm-4">
+			Pellentesque id arcu purus. 
+			Aliquam erat volutpat. Duis viverra porta nisl in porta. 
+			Sed quis lacus vitae massa facilisis euismod et et dui. 
+			Nunc tincidunt, quam id semper iaculis, dui justo posuere dui, hendrerit lacinia ligula lectus quis ex. 
+			Donec vehicula magna eget mi molestie auctor.
+		</div>
+		<div class="col-sm-4">
+			Vestibulum lobortis odio non cursus molestie. 
+			Quisque consectetur libero sapien, at feugiat arcu cursus sit amet. 
+			Curabitur eros nisi, rutrum ornare elementum vitae, malesuada eu metus.
+		</div>
+	</div>
+</div><!-- /box-03-titulo -->
+
+
+<div class="mapa-suporte-tec container">
+	<div class="row">
+		<div class="col-sm-6">
+			<h4>Suporte técnico</h4>
+			<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Integer ac dui ultricies, ultrices diam a, facilisis risus. 
+			Cras sit amet augue sed diam convallis finibus. 
+			Maecenas consectetur ullamcorper velit, ac sodales risus viverra in. 
+			Maecenas faucibus mauris ac lacus ultrices ornare. 
+			Vestibulum imperdiet tellus tristique elit mattis, ac convallis felis luctus. 
+			Aenean sollicitudin facilisis quam sit amet sagittis. 
+			Ut sollicitudin in magna eu rutrum. 
+			Phasellus sed lacus at tortor scelerisque rutrum in eget quam.
+			</p>
+			
+			<img alt="Mapa do suporte técnico" src="img/mapa.PNG" class="img-rounded" width="100%">
+			
+		</div>
+		<div class="col-sm-6 lista">
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			
+			</div>
+			<div class="col-sm-6">
+				<p class="nome-estado">Rio de Janeiro</p>
+				<p class="local">Local 01</p>
+				<p class="local">Local 02</p>
+				<p class="local">Local 03</p>	
+			</div>
+
+		</div><!-- /lista -->
+	</div><!-- row -->
+</div><!-- /mapa-suporte-tec -->
+
+<div class="box-02 container">
+	<div class="row">
+		<div class="col-sm-6">
+			<h4>Nossa Missão</h4>
+			<p>
+				Mauris pretium massa augue, quis dapibus erat interdum ac. 
+				Fusce blandit, metus eget hendrerit venenatis, 
+				dui lectus porta lorem, tempor malesuada ex massa sed erat. 
+				Phasellus eu magna ac enim ultrices molestie. 
+			</p>
+		</div>
+		<div class="col-sm-6">
+			<h4>Nossos produtos</h4>
+			<p>
+				Mauris pretium massa augue, quis dapibus erat interdum ac. 
+				Fusce blandit, metus eget hendrerit venenatis, 
+				dui lectus porta lorem, tempor malesuada ex massa sed erat. 
+				Phasellus eu magna ac enim ultrices molestie.
+			</p>
+		</div>
+	</div><!-- row -->
+	
+</div><!-- /box-02 -->
+
+<?php include '_footer.php';?>
