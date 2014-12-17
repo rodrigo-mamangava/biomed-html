f5073fed7910c6001ac82bb4573ff943c078e861
rodrigo-mamangava <rodrigo@mamangava.com>
2014-12-17 02:15:27 -0200

    Separando _item-equip-descricao.php

---
+++ _item-equip-descricao.php
@@ -0,0 +1,53 @@
+<div class="item-equip-descricao row">
+	<div class="foto-equip col-sm-12 col-md-3 ">
+		<img alt="" src="img/equip/equipamento-01.PNG" width="100%">
+	</div>
+	<!-- foto-equip -->
+	<div class="info-equip col-sm-12 col-md-9">
+		<h1>Nome do equipamento</h1>
+
+		<div class="texto-destaque">
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+				mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et
+				libero. Donec varius erat nisi. Sed eget est purus. Sed tellus arcu,
+				placerat at interdum sed, blandit in dolor.</p>
+
+		</div>
+
+		<div class="texto-descritivo">
+
+			<h2>Sub-titulo</h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+				mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et
+				libero. Donec varius erat nisi. Sed eget est purus. Sed tellus arcu,
+				placerat at interdum sed, blandit in dolor.</p>
+
+			<h2>Sub-titulo</h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+				mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et
+				libero. Donec varius erat nisi. Sed eget est purus. Sed tellus arcu,
+				placerat at interdum sed, blandit in dolor.</p>
+
+			<h2>Sub-titulo</h2>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
+				mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et
+				libero. Donec varius erat nisi. Sed eget est purus. Sed tellus arcu,
+				placerat at interdum sed, blandit in dolor.</p>
+
+			<h2>Sub-titulo</h2>
+			<h2>Sub-titulo</h2>
+			<h2>Sub-titulo</h2>
+
+
+		</div>
+
+
+		<a class="btn-orcamento" href="equipamento.php"> Peça uma orçamento </a>
+	</div>
+	<!-- info-equip -->
+</div>
+<!-- item-equip-descricao -->
--- equipamento.php
+++ equipamento.php
@@ -13,68 +13,7 @@
 	
 	<div class="equipamento col-md-8">
 		
-		<div class="item-equip row">
-			<div class="foto-equip col-sm-12 col-md-3 ">
-				<img alt="" src="img/equip/equipamento-01.PNG" width="100%">
-			</div>
-			<!-- foto-equip -->
-			<div class="info-equip col-sm-12 col-md-9">
-				<h1>Nome do equipamento</h1>
-				
-				<div class="texto-destaque">
-				
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-					mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et libero.
-					Donec varius erat nisi. Sed eget est purus. Sed tellus arcu, placerat
-					at interdum sed, blandit in dolor.
-					</p>
-					
-				</div>
-				
-				<div class="texto-descritivo">
-				
-				<h2>Sub-titulo</h2>
-				
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-					mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et libero.
-					Donec varius erat nisi. Sed eget est purus. Sed tellus arcu, placerat
-					at interdum sed, blandit in dolor.
-				</p>
-				
-				<h2>Sub-titulo</h2>
-				
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-					mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et libero.
-					Donec varius erat nisi. Sed eget est purus. Sed tellus arcu, placerat
-					at interdum sed, blandit in dolor.
-				</p>
-				
-				<h2>Sub-titulo</h2>
-				
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-					mollis nunc. Nullam vel nunc ut mauris pharetra mollis sed et libero.
-					Donec varius erat nisi. Sed eget est purus. Sed tellus arcu, placerat
-					at interdum sed, blandit in dolor.
-				</p>
-				
-				<h2>Sub-titulo</h2>
-				<h2>Sub-titulo</h2>
-				<h2>Sub-titulo</h2>
-				
-				
-				</div>
-				
-		
-				<a class="btn-orcamento" href="equipamento.php">
-					Peça uma orçamento
-				</a>
-			</div>
-			<!-- info-equip -->
-		</div><!-- item-equip -->
+		<?php include '_item-equip-descricao.php';?>
 		
 		<?php include '_vitrine-relacionado.php';?>
 		
